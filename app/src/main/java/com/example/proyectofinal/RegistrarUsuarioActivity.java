70fb1e3f90a696a9954a75d42f22f3d8ea3faee3
WINDOWS 10- <i72775851@isise.edu.pe>
2022-09-19 19:51:35 -0500

    Initial commit

---
+++ app/src/main/java/com/example/proyectofinal/RegistrarUsuarioActivity.java
@@ -0,0 +1,228 @@
+package com.example.proyectofinal;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class RegistrarUsuarioActivity extends AppCompatActivity {
+    private File f;
+    private Button btnSubirImagen, btnGuardarDatos;
+    private CircleImageView imageUser;
+    private AutoCompleteTextView dropdownTipoDoc, dropdownDepartamento, dropdownProvincia, dropdownDistrito;
+    private EditText edtNameUser, edtApellidoPaternoU, edtApellidoMaternoU, edtNumDocU, edtTelefonoU,
+            edtDireccionU;
+    private TextInputLayout txtInputNameUser, txtInputApellidoPaternoU, txtInputApellidoMaternoU,
+            txtInputTipoDoc, txtInputNumeroDocU, txtInputDepartamento, txtInputProvincia,
+            txtInputDistrito, txtInputTelefonoU, txtInputDireccionU,txtUsuario, txtContraseña;
+    private final static int LOCATION_REQUEST_CODE = 23;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar_usuario);
+        this.init();
+        this.initViewModel();
+    }
+
+    private void initViewModel() {
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) !=
+                PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    LOCATION_REQUEST_CODE);
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull @NotNull String[] permissions, @NonNull @NotNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case LOCATION_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Gracias por conceder los permisos para " +
+                            "leer el almacenamiento, estos permisos son necesarios para poder " +
+                            "escoger tu foto de perfil", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "No podemos realizar el registro si no nos concedes los permisos para leer el almacenamiento.", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+    private void init(){
+        btnGuardarDatos = findViewById(R.id.btnGuardarDatos);
+        btnSubirImagen = findViewById(R.id.btnSubirImagen);
+        imageUser = findViewById(R.id.imageUser);
+        edtNameUser = findViewById(R.id.edtNameUser);
+        edtApellidoPaternoU = findViewById(R.id.edtApellidoPaternoU);
+        edtApellidoMaternoU = findViewById(R.id.edtApellidoMaternoU);
+        edtNumDocU = findViewById(R.id.edtNumDocU);
+        edtTelefonoU = findViewById(R.id.edtTelefonoU);
+        edtDireccionU = findViewById(R.id.edtDireccionU);
+        //AutoCompleteTextView
+        dropdownTipoDoc = findViewById(R.id.dropdownTipoDoc);
+        dropdownDepartamento = findViewById(R.id.dropdownDepartamento);
+        dropdownProvincia = findViewById(R.id.dropdownProvincia);
+        dropdownDistrito = findViewById(R.id.dropdownDistrito);
+        //TextInputLayout
+        txtInputNameUser = findViewById(R.id.txtInputNameUser);
+        txtInputApellidoPaternoU = findViewById(R.id.txtInputApellidoPaternoU);
+        txtInputApellidoMaternoU = findViewById(R.id.txtInputApellidoMaternoU);
+        txtInputTipoDoc = findViewById(R.id.txtInputTipoDoc);
+        txtInputNumeroDocU = findViewById(R.id.txtInputNumeroDocU);
+        txtInputDepartamento = findViewById(R.id.txtInputDepartamento);
+        txtInputProvincia = findViewById(R.id.txtInputProvincia);
+        txtInputDistrito = findViewById(R.id.txtInputDistrito);
+        txtInputTelefonoU = findViewById(R.id.txtInputTelefonoU);
+        txtInputDireccionU = findViewById(R.id.txtInputDireccionU);
+        txtUsuario = findViewById(R.id.txtUsuario);
+        txtContraseña = findViewById(R.id.txtContraseña);
+        btnSubirImagen.setOnClickListener(v -> {
+            this.cargarImagen();
+        });
+    }
+
+    private void cargarImagen() {
+        Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        i.setType("image/");
+        startActivityForResult(Intent.createChooser(i, "Seleccione la Aplicación"), 10);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            Uri uri = data.getData();
+            final String realPath = getRealPathFromURI(uri);
+            this.f = new File(realPath);
+            this.imageUser.setImageURI(uri);
+        }
+    }
+
+    private String getRealPathFromURI(Uri contentUri) {
+        String result;
+        Cursor cursor = getContentResolver().query(contentUri, null, null, null, null);
+        if (cursor == null) {
+            result = contentUri.getPath();
+        } else {
+            cursor.moveToFirst();
+            int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+            result = cursor.getString(idx);
+            cursor.close();
+        }
+        return result;
+    }
+
+
+    private boolean validar() {
+        boolean retorno = true;
+        String nombres, apellidoPaterno, apellidoMaterno, numDoc, telefono, direccion,
+                dropTipoDoc, dropDepartamento, dropProvincia, dropDistrito;
+        nombres = edtNameUser.getText().toString();
+        apellidoPaterno = edtApellidoPaternoU.getText().toString();
+        apellidoMaterno = edtApellidoMaternoU.getText().toString();
+        numDoc = edtNumDocU.getText().toString();
+        telefono = edtTelefonoU.getText().toString();
+        direccion = edtDireccionU.getText().toString();
+        dropTipoDoc = dropdownTipoDoc.getText().toString();
+        dropDepartamento = dropdownDepartamento.getText().toString();
+        dropProvincia = dropdownProvincia.getText().toString();
+        dropDistrito = dropdownDistrito.getText().toString();
+        if (this.f == null) {
+            errorMessage("debe selecionar una foto de perfil");
+            retorno = false;
+        }
+        if (nombres.isEmpty()) {
+            txtInputNameUser.setError("Ingresar nombres");
+            retorno = false;
+        } else {
+            txtInputNameUser.setErrorEnabled(false);
+        }
+        if (apellidoPaterno.isEmpty()) {
+            txtInputApellidoPaternoU.setError("Ingresar apellido paterno");
+            retorno = false;
+        } else {
+            txtInputApellidoPaternoU.setErrorEnabled(false);
+        }
+        if (apellidoMaterno.isEmpty()) {
+            txtInputApellidoMaternoU.setError("Ingresar apellido materno");
+            retorno = false;
+        } else {
+            txtInputApellidoMaternoU.setErrorEnabled(false);
+        }
+        if (numDoc.isEmpty()) {
+            txtInputNumeroDocU.setError("Ingresar número documento");
+            retorno = false;
+        } else {
+            txtInputNumeroDocU.setErrorEnabled(false);
+        }
+        if (telefono.isEmpty()) {
+            txtInputTelefonoU.setError("Ingresar número telefónico");
+            retorno = false;
+        } else {
+            txtInputTelefonoU.setErrorEnabled(false);
+        }
+        if (direccion.isEmpty()) {
+            txtInputDireccionU.setError("Ingresar dirección de su casa");
+            retorno = false;
+        } else {
+            txtInputDireccionU.setErrorEnabled(false);
+        }
+        if (dropTipoDoc.isEmpty()) {
+            txtInputTipoDoc.setError("Seleccionar Tipo Doc");
+            retorno = false;
+        } else {
+            txtInputTipoDoc.setErrorEnabled(false);
+        }
+        if (dropDepartamento.isEmpty()) {
+            txtInputDepartamento.setError("Seleccionar Departamento");
+            retorno = false;
+        } else {
+            txtInputDepartamento.setErrorEnabled(false);
+        }
+        if (dropProvincia.isEmpty()) {
+            txtInputProvincia.setError("Seleccionar Provincia");
+            retorno = false;
+        } else {
+            txtInputProvincia.setErrorEnabled(false);
+        }
+        if (dropDistrito.isEmpty()) {
+            txtInputDistrito.setError("Seleccionar Distrito");
+            retorno = false;
+        } else {
+            txtInputDistrito.setErrorEnabled(false);
+        }
+        return retorno;
+    }
+
+    private void errorMessage(String s) {
+    }
+
+}
+
+
---
+++ app/src/main/java/com/example/proyectofinal/entity/service/Cliente.java
@@ -0,0 +1,98 @@
+package com.example.proyectofinal.entity.service;
+
+public class Cliente {
+    private int id;
+    private String nombres;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    private String tipoDoc;
+    private String numDoc;
+    private String departamento;
+    private String provincia;
+    private String distrito;
+    private String telefono;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public String getTipoDoc() {
+        return tipoDoc;
+    }
+
+    public void setTipoDoc(String tipoDoc) {
+        this.tipoDoc = tipoDoc;
+    }
+
+    public String getNumDoc() {
+        return numDoc;
+    }
+
+    public void setNumDoc(String numDoc) {
+        this.numDoc = numDoc;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    public String getDistrito() {
+        return distrito;
+    }
+
+    public void setDistrito(String distrito) {
+        this.distrito = distrito;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    public String getNombreCompleto(){
+        return this.nombres != null && this.apellidoPaterno != null && this.apellidoMaterno != null ?
+                this.nombres + " " + this.apellidoPaterno + " " + this.apellidoMaterno : "------";
+    }
+}
